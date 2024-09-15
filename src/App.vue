9179a3a39b83ea29a081f7c07f714000bc78e1cd
Sachin <leavethesq@gmail.com>
2024-09-15 11:08:42 +0530

    --

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,29 @@
 <template>
   <h1>{{ topic }}</h1>
+  <input type="text" ref="name">
+  <button @click="handleClick" >Click</button>
+
+  <CustomModal />
 </template>
 
 <script>
+import CustomModal from './components/CustomModal.vue';
 
 export default {
   name: 'App',
+  components:{
+    CustomModal
+  },
   data(){
     return{
       topic:'VUE LEARN'
+    }
+  },
+  methods:{
+    handleClick(){
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
     }
   }
   
